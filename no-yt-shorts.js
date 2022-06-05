4d145d7280ff4d690f141b1af6cbf69dba5265c0
andrewdownie <andrew_dev@outlook.com>
2022-06-04 20:28:22 -0400

    Youtube is a SPA, so the script needs to start running even on pages where it has nothing to do
    because if the user starts on a page where there's nothing to do and navigates to the youtube home page, the tampermonkey script won't get loaded due to the SPA nature of youtube

--- no-yt-shorts.js
+++ no-yt-shorts.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.youtube.com
+// @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
